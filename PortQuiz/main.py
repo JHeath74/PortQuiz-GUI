390e0e8a4b68810183a08cef0581190f1d622d5c
JHeath74 <JHeath74@msn.com>
2023-03-08 14:03:36 -0700

    Update main.py
    Get a randomzied value and added to a random prompt value.
    
    Bugs:
    
    after playing, if you exit then reenter,  the dict is empty.
    
    When you select 0 to exit the game,  errors,  then exits game.

--- PortQuiz/main.py
+++ PortQuiz/main.py
@@ -3,14 +3,9 @@ import random
 
 portpoints = 0
 guess = -1
-one = 0
-two = 0
-three = 0
-four = 0
 shuffledvalues = {}
 
 while guess != 0:
-    guess = 0
 
     print("\nPort Quiz")
     print("You'll be given a program and have to guess the port number\n")
@@ -24,54 +19,45 @@ while guess != 0:
     value3 = random.choice(list(PortDictInformation.portDict.keys()))
     value4 = random.choice(list(PortDictInformation.portDict.keys()))
 
-    shuffledvalues = {
-        one: value,
-        two: value2,
-        three: value3,
-        four: value4
-    }
-    if value == value2 or value3 or value4:
-        value = random.choice(list(PortDictInformation.portDict.keys()))
-    elif value2 == value or value3 or value4:
-        value2 = random.choice(list(PortDictInformation.portDict.keys()))
-    elif value3 == value or value2 or value4:
-        value3 = random.choice(list(PortDictInformation.portDict.keys()))
-    elif value4 == value or value2 or value3:
-        value4 = random.choice(list(PortDictInformation.portDict.keys()))
+    shuffledvalues[value] = value
+    shuffledvalues[value2] = value2
+    shuffledvalues[value3] = value3
+    shuffledvalues[value4] = value4
 
-    if one == PortDictInformation.portDict.keys():
-        shuffledvalues.pop(one)
-        one = -1
-    elif two == PortDictInformation.portDict.keys():
-        shuffledvalues.pop(two)
-        two = -1
-    elif three == PortDictInformation.portDict.keys():
-        shuffledvalues.pop(three)
-        three = -1
-    elif four == PortDictInformation.portDict.keys():
-        shuffledvalues.pop(four)
-        four = -1
+    newvalue = random.choice(list(shuffledvalues.keys()))
+    shuffledvalues.pop(newvalue)
+    newvalue2 = random.choice(list(shuffledvalues.keys()))
+    shuffledvalues.pop(newvalue2)
+    newvalue3 = random.choice(list(shuffledvalues.keys()))
+    shuffledvalues.pop(newvalue3)
+    newvalue4 = random.choice(list(shuffledvalues.keys()))
+    shuffledvalues.pop(newvalue4)
 
     guess = input("Which of the following ports is used by " + portinfo + ":\n"
-                  + "Port " + str(value) + "\n"
-                  + "Port " + str(value2) + "\n"
-                  + "Port " + str(value3) + "\n"
-                  + "Port " + str(value4) + "\n"
-                  + "Type 0 to exit"
-                  + "\nWhich port is your guess?\n "
+                  + "Port " + str(newvalue) + "\n"
+                  + "Port " + str(newvalue2) + "\n"
+                  + "Port " + str(newvalue3) + "\n"
+                  + "Port " + str(newvalue4) + "\n"
+                  + "Type 0 to exit\n"
+                  + "\nWhich port is your guess?\n"
                   + "Answer is: ")
 
-    if guess == value:
+    if guess == 0:
+        break
+    elif guess == value:
         print("Congratulations, your right!")
         portpoints += 1
         print("Your Current Score: " + str(portpoints))
-    elif guess == 0:
-        break
+        print("______________________________________")
+        print("______________________________________")
+
     else:
         print("\nYour choice was not correct.\n"
               "The correct answer is " + value)
         portpoints -= 1
         print("Your Current Score: " + str(portpoints))
+        print("______________________________________")
+        print("______________________________________")
 
 print("Quiz Over")
 print("Total Score: " + str(portpoints))
