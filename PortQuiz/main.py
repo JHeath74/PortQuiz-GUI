bc0d067c039451499b3ccead568c099d3a28be77
JHeath74 <JHeath74@msn.com>
2023-04-03 14:35:48 -0600

    Just trying to get it to work

--- PortQuiz/main.py
+++ PortQuiz/main.py
@@ -1,18 +1,19 @@
-import random
-
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
 import sys
 
-from PortQuiz import PortQuizAwards, PortDictInformation
 
-#https://nitratine.net/blog/post/how-to-import-a-pyqt5-ui-file-in-a-python-gui/#importing-the-ui-file-in-python
 class PortQuiz(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(PortQuiz, self).__init__()
-        uic.loadUi('PortQuizGame.ui', self)
-        self.show()
 
-app = QtWidgets.QApplication(sys.argv)
-window = PortQuiz()
-app.exec_()
+	def __init__(self):
+		super().__init__()
 
+		self.setWindowTitle("Port Quiz")
+		screen_resolution = app.desktop().screenGeometry()
+		self.setGeometry(screen_resolution)
+		self.show()
+
+
+if __name__ == '__main__':
+	app = QtWidgets.QApplication(sys.argv)
+	w = PortQuiz()
+	app.exec()
