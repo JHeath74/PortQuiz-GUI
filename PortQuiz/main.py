6237fb395fa30400f12ca5d1313ebacc857c5ced
JHeath74 <JHeath74@msn.com>
2023-04-04 10:08:17 -0600

    Updated Main.py
    Added Fields and Buttons.  Buttons are clickable

--- PortQuiz/main.py
+++ PortQuiz/main.py
@@ -1,19 +1,104 @@
-from PyQt5 import QtWidgets
 import sys
 
 
-class PortQuiz(QtWidgets.QMainWindow):
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QPlainTextEdit
+from PyQt5.QtCore import pyqtSlot
+
+
+class PortQuiz(QWidget):
 
 	def __init__(self):
 		super().__init__()
+		self.title = 'Port Quiz'
 
-		self.setWindowTitle("Port Quiz")
-		screen_resolution = app.desktop().screenGeometry()
-		self.setGeometry(screen_resolution)
+		self.setFixedHeight(700)
+		self.setFixedWidth(1300)
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle(self.title)
+
+		# Width,  then Height
+
+		Port1 = QPushButton('Port 1:', self)
+		Port1.setToolTip('Answer Choice 1')
+		Port1.move(50, 500)
+		Port1.resize(200, 100)
+		Port1.clicked.connect(self.Port_1_on_click)
+
+		Port2 = QPushButton('Port 2:', self)
+		Port2.setToolTip('Answer Choice 2')
+		Port2.move(250, 500)
+		Port2.resize(200, 100)
+		Port2.clicked.connect(self.Port_2_on_click)
+
+		Port3 = QPushButton('Port 3:', self)
+		Port3.setToolTip('Answer Choice 3')
+		Port3.move(450, 500)
+		Port3.resize(200, 100)
+		Port3.clicked.connect(self.Port_3_on_click)
+
+		Port4 = QPushButton('Port 4:', self)
+		Port4.setToolTip('Answer Choice 4')
+		Port4.move(650, 500)
+		Port4.resize(200, 100)
+		Port4.clicked.connect(self.Port_4_on_click)
+
+		# Number of Correct Answers Box
+		# Width,  then Height
+		self.NumberofCorrectAnswers = QLineEdit(self)
+		self.NumberofCorrectAnswers.move(900, 100)
+		self.NumberofCorrectAnswers.resize(300, 75)
+		self.NumberofCorrectAnswers.isReadOnly()
+
+		self.NumberOfCorrectAnswersLabel = QLabel(self)
+		self.NumberOfCorrectAnswersLabel.setText("Number of Correct Answers")
+		self.NumberOfCorrectAnswersLabel.move(900, 50)
+
+		self.NumberofInCorrectAnswers = QLineEdit(self)
+		self.NumberofInCorrectAnswers.move(900, 250)
+		self.NumberofInCorrectAnswers.resize(300, 75)
+		self.NumberofInCorrectAnswers.isReadOnly()
+
+		self.NumberofInCorrectAnswersLabel = QLabel(self)
+		self.NumberofInCorrectAnswersLabel.setText("Number of Incorrect Answers")
+		self.NumberofInCorrectAnswersLabel.move(900, 175)
+		self.NumberofInCorrectAnswersLabel.resize(300, 75)
+
+		self.Score = QLineEdit(self)
+		self.Score.move(900, 400)
+		self.Score.resize(300, 75)
+		self.Score.isReadOnly()
+
+		self.ScoreLabel = QLabel(self)
+		self.ScoreLabel.setText("Current Score")
+		self.ScoreLabel.move(900, 350)
+		self.Score.resize(300, 75)
+
+		self.DisplayQuestionField = QPlainTextEdit(self)
+		self.DisplayQuestionField.move(50, 50)
+		self.DisplayQuestionField.resize(800, 425)
+
 		self.show()
+
+	@pyqtSlot()
+	def Port_1_on_click(self):
+		print('Port 1')
+
+	@pyqtSlot()
+	def Port_2_on_click(self):
+		print('Port 2')
+
+	@pyqtSlot()
+	def Port_3_on_click(self):
+		print('Port 3')
+
+	@pyqtSlot()
+	def Port_4_on_click(self):
+		print('Port 4')
 
 
 if __name__ == '__main__':
-	app = QtWidgets.QApplication(sys.argv)
-	w = PortQuiz()
-	app.exec()
+	app = QApplication(sys.argv)
+	ex = PortQuiz()
+	sys.exit(app.exec_())
